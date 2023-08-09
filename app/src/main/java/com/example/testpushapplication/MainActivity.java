52dfd6ced677a0d5033865720a1c058eaa8f30ec
aravind <aravindraj2018@gmail.com>
2023-08-09 08:55:40 +0530

    test commit 1

---
+++ app/src/main/java/com/example/testpushapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.testpushapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // test push project
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // restarting myself to complete the incomplete things
+    }
+}
